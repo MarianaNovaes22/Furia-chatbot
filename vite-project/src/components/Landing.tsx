e749a840ae714024e44a57edaf8b3ad3e3bca120
Mariana <mariana.c.novaes22@gmail.com>
2025-04-28 03:51:02 -0300

    style: Adicionado fundo, animação de seta e logo da FURIA na landing page

--- vite-project/src/components/Landing.tsx
+++ vite-project/src/components/Landing.tsx
@@ -1,9 +1,24 @@
+import { Link } from "react-scroll";
+import BackgroundLanding from "../assets/imagens/background-landing.jpg";
+import LogoFuria from "../assets/imagens/furia-logo.svg";
+
 const Landing = () => {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen bg-blue-500">
-        <h1 className="text-white text-4xl">Bem-vindo ao Chatbot!</h1>
-      </div>
-    );
-  };
-  
-  export default Landing;
+  return (
+    <div
+      className="h-screen w-full bg-cover bg-center relative flex flex-col justify-center items-center"
+      style={{ backgroundImage: `url(${BackgroundLanding})` }}
+    >
+      {/* Logo */}
+      <img src={LogoFuria} alt="Logo Furia" className="w-48 md:w-64" />
+
+      {/* Botão de Scroll */}
+      <Link to="chatbot" smooth={true} duration={800}>
+        <button className="absolute bottom-10 text-white text-2xl animate-bounce">
+          ↓
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default Landing;
