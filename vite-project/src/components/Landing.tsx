ebd437679a9594836079994fe820d9ca2b9015e1
Mariana <mariana.c.novaes22@gmail.com>
2025-04-28 03:58:12 -0300

    style: Adicionando mais ícones

--- vite-project/src/components/Landing.tsx
+++ vite-project/src/components/Landing.tsx
@@ -1,22 +1,39 @@
 import { Link } from "react-scroll";
 import BackgroundLanding from "../assets/imagens/background-landing.jpg";
-import LogoFuria from "../assets/imagens/furia-logo.svg";
+import LogoFuriaLobo from "../assets/imagens/furia-logo2.svg"; 
+import LogoFuriaTexto from "../assets/imagens/furia-logo.svg"; 
+
+import { FaUser } from "react-icons/fa";
 
 const Landing = () => {
   return (
     <div
-      className="h-screen w-full bg-cover bg-center relative flex flex-col justify-center items-center"
+      className="h-screen w-full bg-cover bg-center relative flex flex-col"
       style={{ backgroundImage: `url(${BackgroundLanding})` }}
     >
-      {/* Logo */}
-      <img src={LogoFuria} alt="Logo Furia" className="w-48 md:w-64" />
+      {/* Header */}
+      <header className="w-full px-8 py-4 flex items-center justify-between">
+        {/* Logo + Nome */}
+        <div className="flex items-center gap-4">
+          <img src={LogoFuriaLobo} alt="Logo Furia Lobo" className="h-10" />
+          <img src={LogoFuriaTexto} alt="Nome Furia" className="h-6" />
+        </div>
 
-      {/* Botão de Scroll */}
-      <Link to="chatbot" smooth={true} duration={800}>
-        <button className="absolute bottom-10 text-white text-2xl animate-bounce">
-          ↓
-        </button>
-      </Link>
+        {/* Ícone de Usuário */}
+        <div>
+          <FaUser className="text-white text-2xl cursor-pointer" />
+        </div>
+      </header>
+
+      {/* Conteúdo principal (logo e botão) */}
+      <div className="flex-grow flex flex-col justify-center items-center">
+        <img src={LogoFuriaTexto} alt="Logo Furia" className="w-48 md:w-64" />
+        <Link to="chatbot" smooth={true} duration={800}>
+          <button className="absolute bottom-10 text-white text-2xl animate-bounce">
+            ↓
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
